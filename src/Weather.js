4c7215a07439c9e9eae62e903c550218363e52f8
Eliani <via.eliani@gmail.com>
2020-08-21 21:08:46 -0400

    Added conversion for imperial vs metric units

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Stats from "./Stats";
 
 export default function Weather(props) {
-  return (
-    <div className="Weather col-6">
-      <span className="temperature">{props.data.temperature}°C</span>
-      <Stats data={props.data} />
-    </div>
-  );
+  const [unit, setUnit] = useState("metric");
+  function showImperial(event) {
+    event.preventDefault();
+    setUnit("imperial");
+  }
+  function showMetric(event) {
+    event.preventDefault();
+    setUnit("metric");
+  }
+
+  if (unit === "metric") {
+    return (
+      <div className="Weather col-6">
+        <span className="temperature">
+          {props.data.temperature}
+          <sup className="unit">
+            °C|
+            <a href="/" onClick={showImperial}>
+              °F
+            </a>
+          </sup>
+        </span>
+        <Stats data={props.data} />
+      </div>
+    );
+  } else {
+    let imperialTemp = Math.round((props.data.temperature * 9) / 5 + 32);
+    return (
+      <div className="Weather col-6">
+        <span className="temperature">
+          {imperialTemp}
+          <sup className="unit">
+            <a href="/" onClick={showMetric}>
+              °C
+            </a>
+            |°F
+          </sup>
+        </span>
+        <Stats data={props.data} />
+      </div>
+    );
+  }
 }
